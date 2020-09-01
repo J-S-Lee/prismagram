7074007e0c86014c3828dd77ce57b18ca21b8291
JSLee <jasoc1004@gmail.com>
2020-09-01 16:05:57 +0900

    seeFullPost refactoring with fragment

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -23,23 +23,5 @@ export default {
             const { id: parentId } = parent;
             return parentId === user.id;
         }
-    },
-    Post: {
-        isLiked: (parent, _, { request }) => {
-            const { user } = request;
-            const { id } = parent;
-            return prisma.$exists.like({
-                AND: [{
-                    user: {
-                        id: user.id
-                    }
-                }, {
-                    post: {
-                        id
-                    }
-                }]
-            })
-        }
-    }
-    
+    }    
 }
